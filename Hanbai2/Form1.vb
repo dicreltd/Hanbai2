de312bdca45c3c4a6e842c70385a2349b32a3013
dicreltd <skohara@dicre.com>
2020-06-12 07:21:28 +0900

    Update Form1.vb
    0612

--- Hanbai2/Form1.vb
+++ Hanbai2/Form1.vb
@@ -38,17 +38,6 @@ Public Class Form1
 
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
-        Dim index = DataGridView1.CurrentCell.RowIndex
-        Dim row = table.Rows(index)
-        TextBox1.Text = row("sname").ToString()
-        TextBox2.Text = row("tanka").ToString()
-
-    End Sub
 
     Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
         Dim index = DataGridView1.CurrentCell.RowIndex
